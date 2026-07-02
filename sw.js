4ce9fab9d54da30a2479b62d403bfee7a8366067
Angel HES 1217 <angeljesus8@gmail.com>
2026-07-02 16:11:20 -0600

    fix: EOL desde nube + exhibe para SKUs fuera de catalogo

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-const CACHE = 'hes1217-v23';
+const CACHE = 'hes1217-v24';
 const ARCHIVOS = [
   './index.html',
   './tablero.html',
