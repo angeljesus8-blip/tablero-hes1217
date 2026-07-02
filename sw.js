1b1934a3df3239a74c5f46b2ee9d9789959f5c3c
Angel HES 1217 <angeljesus8@gmail.com>
2026-07-02 17:52:20 -0600

    feat: mostrar 18 MSI y 12 MSI juntos para compras mayores a 20000

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-const CACHE = 'hes1217-v29';
+const CACHE = 'hes1217-v30';
 const ARCHIVOS = [
   './index.html',
   './tablero.html',
