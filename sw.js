fbd26fc03d8d94c92b220c8c3e0ecb7d95d6bda1
Angel HES 1217 <angeljesus8@gmail.com>
2026-07-02 19:27:13 -0600

    fix double callback in cargarListaAvisos, bump SW v39

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 importScripts('https://cdn.onesignal.com/sdks/web/v16/OneSignalSDK.sw.js');
-const CACHE = 'hes1217-v38';
+const CACHE = 'hes1217-v39';
 const ARCHIVOS = [
   './index.html',
   './tablero.html',
